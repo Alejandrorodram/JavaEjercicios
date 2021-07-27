92f9f3e966080804f905d634d2b680c96bbd6ee7
Alejandrorodram <ale1.zul@gmail.com>
2021-07-27 15:02:27 +0100

    test commit 2

---
+++ src/Alejandrorodram/JavaEjercicios/Udemy/KmToMiles.java
@@ -0,0 +1,28 @@
+package Alejandrorodram.JavaEjercicios.Udemy;
+
+public class KmToMiles {
+
+    public static long toMilesPerHour (double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+        else {
+            long milesPerHour = Math.round(kilometersPerHour / 1.609);
+            return milesPerHour;
+        }
+    }
+    public static void printConversion (double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        }
+        else {
+            long miles = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + miles + " mi/h");
+        }
+    }
+    public static void main(String[] args) {
+        //example 50km to miles)
+        printConversion(50);
+    }
+
+}
